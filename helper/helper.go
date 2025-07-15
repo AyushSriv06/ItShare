b4f68298650cc9fa17b38cb9b107093972e8f2de
nishant1479 <nishantdubey147@gmail.com>
2025-07-15 14:48:51 +0530

    feat: Server Availability Check

---
+++ helper/helper.go
@@ -0,0 +1,24 @@
+package helper
+
+import (
+	"net"
+	"strings"
+	"time"
+)
+
+func CheckServerAvailability(address string) (bool, string) {
+	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+	if err != nil {
+		// Provide more specific error information
+		if strings.Contains(err.Error(), "connection refused") {
+			return false, "Connection refused - no server running at this address"
+		} else if strings.Contains(err.Error(), "no such host") {
+			return false, "Host not found - check if the hostname is correct"
+		} else if strings.Contains(err.Error(), "i/o timeout") {
+			return false, "Connection timed out - server might be behind a firewall"
+		}
+		return false, err.Error()
+	}
+	conn.Close()
+	return true, ""
+}
