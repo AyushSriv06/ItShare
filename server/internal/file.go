346d2db0d9082c7e4175f1ea6644411843ff31b4
Ayush Srivastava <ayushsrisks@gmail.com>
2025-07-16 15:13:46 +0530

    feat:implemented file trnasfer

--- server/internal/file.go
+++ server/internal/file.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+//sending file metadata including the checksum
 func HandleFileTransfer(server *interfaces.Server, conn net.Conn, recipientId, fileName string, fileSize int64) {
 	checksum := ""
 	fileNameWithChecksum := fileName
@@ -37,5 +38,28 @@ func HandleFileTransfer(server *interfaces.Server, conn net.Conn, recipientId, f
 		}
 	} else {
 		fmt.Printf("User %s not found\n", recipientId)
+	}
+}
+
+//sending the actual file
+func SendFile(server *interfaces.Server, senderId, recipientId, filePath string) {
+	server.Mutex.Lock()
+	defer server.Mutex.Unlock()
+
+	_, exists := server.Connections[recipientId]
+	if !exists {
+		fmt.Printf("User %s not found\n", recipientId)
+		return
+	}
+
+	sender, exists := server.Connections[senderId]
+	if !exists {
+		fmt.Printf("User %s not found\n", senderId)
+		return
+	}
+
+	_, err := sender.Conn.Write([]byte(fmt.Sprintf("/sendfile %s %s\n", recipientId, filePath)))
+	if err != nil {
+		fmt.Printf("Error sending file to %s: %v\n", recipientId, err)
 	}
 }
