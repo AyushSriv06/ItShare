971cee7f4007fa1478771c65e8106e7fd6181170
nishant1479 <nishantdubey147@gmail.com>
2025-07-18 03:52:15 +0530

    feat: Added Lookup Request and Response

--- client/internal/folder.go
+++ client/internal/folder.go
@@ -1,12 +1,12 @@
 package connection
 
 import (
+	"ItShare/helper"
 	"fmt"
 	"io"
 	"net"
-	"path/filepath"
-	"ItShare/helper"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -29,15 +29,13 @@ func HandleFolderTransfer(conn net.Conn, recipientId, folderName string, folderS
 		transferID = GenerateTransferID()
 	}
 
-
 	// Create temporary zip file to store received data
 	tempZipPath := filepath.Join(storeFilePath, folderName+".zip")
 	zipFile, err := os.Create(tempZipPath)
 	if err != nil {
-		fmt.Println( err)
+		fmt.Println(err)
 		return
 	}
-
 
 	// Create transfer record
 	transfer := &Transfer{
@@ -112,4 +110,93 @@ func HandleFolderTransfer(conn net.Conn, recipientId, folderName string, folderS
 	os.Remove(tempZipPath)
 
 	RemoveTransfer(transferID)
+}
+
+func HandleLookupRequest(conn net.Conn, userId string) {
+	_, err := conn.Write([]byte(fmt.Sprintf("/LOOK %s\n", userId)))
+	if err != nil {
+		fmt.Printf("Error sending look request: %v\n", err)
+		return
+	}
+}
+
+func HandleLookupResponse(conn net.Conn, storeFilePath string, userId string) {
+	// Clean and normalize the path
+	cleanPath := filepath.Clean(strings.TrimSpace(storeFilePath))
+	absPath, err := filepath.Abs(cleanPath)
+	if err != nil {
+		fmt.Printf("Error resolving absolute path: %v\n", err)
+		return
+	}
+
+	// Verify directory exists and is accessible
+	info, err := os.Stat(absPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("Store directory does not exist: %s\n", absPath)
+		} else {
+			fmt.Printf("Error accessing directory: %v\n", err)
+		}
+		return
+	}
+
+	if !info.IsDir() {
+		fmt.Printf("Path is not a directory: %s\n", absPath)
+		return
+	}
+
+	var folders []string
+	var files []string
+
+	err = filepath.Walk(absPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("Error accessing path %s: %v\n", path, err)
+			return nil
+		}
+		if path == absPath {
+			return nil
+		}
+
+		// Get clean relative path
+		absolutePath := filepath.ToSlash(path)
+
+		if info.IsDir() {
+			folders = append(folders, fmt.Sprintf("[FOLDER] %s (Size: %d bytes)", absolutePath, info.Size()))
+		} else {
+			files = append(files, fmt.Sprintf("[FILE] %s (Size: %d bytes)", absolutePath, info.Size()))
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking directory: %v\n", err)
+		return
+	}
+
+	var allEntries []string
+	if len(folders) > 0 {
+		allEntries = append(allEntries, "=== FOLDERS ===")
+		allEntries = append(allEntries, folders...)
+	}
+	if len(files) > 0 {
+		if len(allEntries) > 0 {
+			allEntries = append(allEntries, "") // Add spacing between folders and files
+		}
+		allEntries = append(allEntries, "=== FILES ===")
+		allEntries = append(allEntries, files...)
+	}
+
+	if len(allEntries) == 0 {
+		allEntries = append(allEntries, "Directory is empty")
+	}
+
+	response := fmt.Sprintf("LOOK_RESPONSE %s %s\n", userId, strings.Join(allEntries, "\n"))
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Printf("Error sending lookup response: %v\n", err)
+	}
+
+	for _, entry := range allEntries {
+		fmt.Println(entry)
+	}
 }
