5af604f513ffa0b4c59397f28aa9655357a881ef
Ayush Srivastava <ayushsrisks@gmail.com>
2025-07-17 16:38:37 +0530

    feat: added-checksum

--- helper/helper.go
+++ helper/helper.go
@@ -1,12 +1,54 @@
 package helper
 
 import (
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"io"
 	"math/rand"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
+
+//computes an MD5 hash of a file
+func CalculateFileChecksum(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+// computes an MD5 hash from a reader without consuming it
+// Returns the checksum and a new reader that can be used normally
+func CalculateDataChecksum(reader io.Reader) (string, io.Reader, error) {
+	hash := md5.New()
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return "", nil, err
+	}
+	
+	hash.Write(data)
+	checksum := hex.EncodeToString(hash.Sum(nil))
+	
+	// Return a new reader with the same data
+	return checksum, bytes.NewReader(data), nil
+}
+
+// checks if two checksums match
+func VerifyChecksum(original, received string) bool {
+	return original == received
+}
 
 func CheckServerAvailability(address string) (bool, string) {
 	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
